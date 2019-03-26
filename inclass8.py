acd4ac895f985fc35e6ba6de471bd567d302ba88
Ryan Rayes <rrayes@student.waubonsee.edu>
2019-03-26 11:50:58 -0500

    add 10 numbers

--- inclass8.py
+++ inclass8.py
@@ -28,6 +28,36 @@ while user_num5 > 10000 or user_num5 <0:
     print("Invalid Number")
     user_num5 = int(input("Please enter first number between 0-10000: "))
 
+user_num6 = int(input("Please enter sixth number between 0-10000: "))
+
+while user_num6 > 10000 or user_num6 <0:
+    print("Invalid Number")
+    user_num6 = int(input("Please enter first number between 0-10000: "))
+
+user_num7 = int(input("Please enter seventh number between 0-10000: "))
+
+while user_num7 > 10000 or user_num7 <0:
+    print("Invalid Number")
+    user_num7 = int(input("Please enter first number between 0-10000: "))
+
+user_num8 = int(input("Please enter eighth number between 0-10000: "))
+
+while user_num8 > 10000 or user_num8 <0:
+    print("Invalid Number")
+    user_num8 = int(input("Please enter first number between 0-10000: "))
+
+user_num9 = int(input("Please enter ninth number between 0-10000: "))
+
+while user_num9 > 10000 or user_num9 <0:
+    print("Invalid Number")
+    user_num9 = int(input("Please enter first number between 0-10000: "))
+
+user_num10 = int(input("Please enter tenth number between 0-10000: "))
+
+while user_num10 > 10000 or user_num10 <0:
+    print("Invalid Number")
+    user_num10 = int(input("Please enter first number between 0-10000: "))
+
 
 
 
@@ -42,7 +72,17 @@ if user_num4 > max:
     max = user_num4
 if user_num5 > max:
     max = user_num5
-print(max)
+if user_num6 > max:
+    max = user_num6
+if user_num7 > max:
+    max = user_num7
+if user_num8 > max:
+    max = user_num8
+if user_num9 > max:
+    max = user_num9
+if user_num10 > max:
+    max = user_num10
+print('Max: ',max)
 
 min = user_num2
 
@@ -54,7 +94,17 @@ if user_num4 < min:
     min = user_num4
 if user_num5 < min:
     min = user_num5
-print(min)
+if user_num6 < min:
+    min = user_num6
+if user_num7 < min:
+    min = user_num7
+if user_num8 < min:
+    min = user_num8
+if user_num9 < min:
+    min = user_num9
+if user_num10 < min:
+    min = user_num10
+print('Min: ', min)
 
 
 
